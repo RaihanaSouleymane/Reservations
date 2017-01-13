a07a5630f11e73da8564994518f157cf75cd8b7c
Macbook <macbook@macbooks-mbp>
2017-01-13 14:23:42 -0800

    Initial Commit

---
+++ Reservations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reservations
+//
+//  Created by Macbook on 1/13/17.
+//  Copyright © 2017 Raihana A. Souleymane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
